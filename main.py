0aa47c7bff45074ac9b703661125c4164235e326
mothsnyx <edenexssun@gmail.com>
2026-01-31 16:40:12 +0100

    Update main.py

--- main.py
+++ main.py
@@ -131,17 +131,14 @@ class ProfileView(discord.ui.View):
             )
                     # Profile Name
             embed.add_field(
-                name=f"<:15824redneonstar:1467170916017639615> ┃ {self.profile_data['name']}'s Profile",
-                value="",
-                inline=False
+                name=f"<:15824redneonstar:1467170916017639615> ┃ {self.profile_data['name']}'s Profile", value="", inline=False
             )
-            
+            embed.add_field(name="‎", value="", inline=False)
             # Role
             embed.add_field(
-                name=".✦ Role:",
-                value=self.profile_data['role'],
-                inline=False
+                name=".✦ Role:", value=self.profile_data['role'], inline=False
             )
+            embed.add_field(name="‎", value="", inline=False)
             
             # Currency
             currency_text = (
@@ -149,9 +146,7 @@ class ProfileView(discord.ui.View):
                 f"<:ac:1467159725870154021> {self.profile_data['auric_cells']:,} ac"
             )
             embed.add_field(
-                name=".✦ Currency:",
-                value=currency_text,
-                inline=False
+                name=".✦ Currency:", value=currency_text, inline=False
             )
             return embed
             
