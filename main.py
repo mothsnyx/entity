415b98c48a52164556e8811b04503b6299e78861
mothsnyx <edenexssun@gmail.com>
2026-03-14 17:44:48 +0100

    Add View and Button imports from discord.ui

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import discord
 from discord import app_commands
 from discord.ext import commands
+from discord.ui import View, Button
 import random
 import re
 from database import Database, ADMIN_IDS
